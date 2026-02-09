4950405c8055acc31b1f4c17fcf880f05a8b4d8b
mohamed <mohamedeng747@gmail.com>
2026-02-09 21:20:08 +0200

    m

--- dom.js
+++ dom.js
@@ -51,5 +51,47 @@ querySelectorAll()
 // DOM Selectors End
 
 
+// Dom imag links forms  Start
 
+
+// console.log(document.documentElement)
+// console.log(document.head)
+// console.log(document.body)
+// console.log(document.title)
+// console.log(document.head.children)
+// console.log(document.head.children[2])
+
+
+
+// console.log(document.links)
+// console.log(document.links[0])
+// console.log(document.links[0].href)
+
+
+// document.links[0].href = "https://github.com"
+
+// document.links[0].innerHTML = "Github"
+
+// console.log(document.links[0].href)
+
+
+
+
+// console.log(document.forms)
+// console.log(document.forms[0])
+// console.log(document.forms[0].Name)
+// console.log(document.forms[0].Email)
+
+
+
+// console.log(document.images)
+// console.log(document.images[0])
+// console.log(document.images[0].src)
+
+// document.images[0].src = "img/belt.webp"
+
+// console.log(document.images[0].src)
+
+
+// Dom imag links forms End
 
